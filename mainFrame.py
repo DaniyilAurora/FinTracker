d0d00378faab24dc954f343ed14878210d2ef7ac
Daniyil Preen <daniyil.aurora@gmail.com>
2025-04-16 17:01:41 +0200

    Added graph to the analytics panel

--- mainFrame.py
+++ mainFrame.py
@@ -1,4 +1,6 @@
 import tkinter as tk
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.figure import Figure
 
 
 class MainFrame(tk.Frame):
@@ -10,8 +12,8 @@ class MainFrame(tk.Frame):
         mainFrame.pack(fill=tk.BOTH, expand=True)
 
         # Left side frame
-        analyticsFrame = tk.Frame(mainFrame, bg="lightgray")
-        analyticsFrame.grid(row=0, column=0, sticky="nsew", padx=5, pady=5)
+        self.analyticsFrame = tk.Frame(mainFrame, bg="white")
+        self.analyticsFrame.grid(row=0, column=0, sticky="nsew", padx=5, pady=5)
 
         # Right side frame
         transactionFrame = tk.Frame(mainFrame, bg="#F0F0F0")
@@ -29,8 +31,25 @@ class MainFrame(tk.Frame):
         mainFrame.grid_columnconfigure(1, weight=1)
 
         # Analytics Section (Top Left Panel)
-        leftLabel = tk.Label(analyticsFrame, text="Analytics", bg="lightgray", font=("Arial", 14))
-        leftLabel.pack(padx=10, pady=10)
+        leftLabel = tk.Label(self.analyticsFrame, text="Analytics", bg="white", font=("Arial", 30))
+        leftLabel.pack(padx=10, pady=15)
+
+        # Create a transaction figure
+        xdata = [0, 2, 3, 4, 5]
+        ydata = [0, 4, 2, 3, 7]
+        fig = Figure(figsize=(5, 4), dpi=100)
+        plot = fig.add_subplot(111)
+        l = plot.fill_between(xdata, ydata)
+        l.set_facecolors([[.5,.5,.8,.3]])
+        plot.plot(xdata, ydata)  # Sample data
+        plot.set_xlabel("Days")
+        plot.set_ylabel("Account Balance")
+        plot.margins(x=0, y=0)
+
+        # Embed the figure in a canvas
+        canvas = FigureCanvasTkAgg(fig, master=self.analyticsFrame)
+        canvas.draw()
+        canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=True)
 
         # Transaction Section (Right Panel)
         self.transactionCanvas = tk.Canvas(transactionFrame, bg="#666666", highlightthickness=0)
@@ -44,7 +63,6 @@ class MainFrame(tk.Frame):
         # Information Section (Bottom Panel)
         bottomLabel = tk.Label(informationFrame, text="Information", bg="#666666", font=("Arial", 14))
         bottomLabel.pack(padx=10, pady=10)
-
         # Layout of canvas and scrollbar
         self.transactionCanvas.pack(side="left", fill="both", expand=True)
         scrollbar.pack(side="right", fill="y")
@@ -56,6 +74,7 @@ class MainFrame(tk.Frame):
         # TEST DATA, TODO: REMOVE LATER
         for i in range(100):
             tk.Label(scrollableFrame, text=f"Transaction {i+1}", bg="#888888", fg="white").pack(fill="x", padx=5, pady=2)
+
 
     def onMousewheel(self, event):
         self.transactionCanvas.yview_scroll(int(-1 * (event.delta / 120)), "units")
